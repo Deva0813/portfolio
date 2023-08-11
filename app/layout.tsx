4d019b7f3bc12c903f84271b0ddf9cf46f6761d9
Deva0813 <devanand151101@gmail.com>
2023-08-11 13:13:16 +0530

    Init Project

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({ 
+  weight:["100","200","300","400","500","600","700","800","900"],
+  style:["normal","italic"],
+  subsets:["latin","latin-ext"],
+ })
+
+export const metadata: Metadata = {
+  title: 'Deva Portfolio',
+  description: 'Deva Portfolio web site',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+import { NavbarSection } from './components/NavbarSection/page'
+export default function Home() {
+  return (
+    <main className='' >
+      <NavbarSection />
+    </main>
+  )
+}
